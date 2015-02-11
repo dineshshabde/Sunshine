ae2a9a7f2310558c6988297836dd80858c6abc40
Dinesh V <dinesh@auctionsoftware.com>
2015-02-11 14:18:27 +0530

    Added code to execute thread from shared preference

--- app/src/main/java/com/auctionsoftware/dinesh/sunshine/ForecastFragment.java
+++ app/src/main/java/com/auctionsoftware/dinesh/sunshine/ForecastFragment.java
@@ -5,9 +5,11 @@ package com.auctionsoftware.dinesh.sunshine;
  */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -65,7 +67,9 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
